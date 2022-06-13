08e725d0d265a00ae808faf5e43863fe6486be48
Samuel Hartmann <hartmann.samuel@gmx.de>
2022-06-13 08:09:18 +0200

    own 'improved' approach (toggle)

--- app.js
+++ app.js
@@ -3,16 +3,28 @@ const closedFace = document.querySelector('.closed')
 const openFace = document.querySelector('.open')
 
 // event listeners
+// tutorial approach before https://youtu.be/mCQ1-iDSnto?t=838
+// closedFace.addEventListener('click', () => {
+//     if(openFace.classList.contains('open')) {
+//         openFace.classList.add('active')
+//         closedFace.classList.remove('active')
+//     }
+// })
+
+// openFace.addEventListener('click', () => {
+//     if(closedFace.classList.contains('closed')) {
+//         closedFace.classList.add('active')
+//         openFace.classList.remove('active')
+//     }
+// })
+
+// own toggle approach
 closedFace.addEventListener('click', () => {
-    if(openFace.classList.contains('open')) {
-        openFace.classList.add('active')
-        closedFace.classList.remove('active')
-    }
+    closedFace.classList.toggle('active')
+    openFace.classList.toggle('active')
 })
 
 openFace.addEventListener('click', () => {
-    if(closedFace.classList.contains('closed')) {
-        closedFace.classList.add('active')
-        openFace.classList.remove('active')
-    }
+    openFace.classList.toggle('active')
+    closedFace.classList.toggle('active')
 })
